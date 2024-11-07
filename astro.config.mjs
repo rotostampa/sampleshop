f5ef301629e6b9aa25820a6c14e2f70add45cb7d
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2024-11-07 11:30:40 +0100

    use sample shop

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({site: "http://www.example.com"});
+export default defineConfig({site: "https://rotostampa.github.io/sampleshop/"});
