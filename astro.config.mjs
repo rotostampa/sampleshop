cfafd069d8133c54c00032704ff4b618914bc833
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2024-11-07 12:42:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sampleshop.rotostampa.com',  // Base URL of the app
+
+  site: 'https://rotostampa.github.io',
+  base: 'sampleshop',
+
 });
